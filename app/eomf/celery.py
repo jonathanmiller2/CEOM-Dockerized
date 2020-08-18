a97feed7d2c4adb106de2f8b0e40c1e855fb18f8
Jonathan Miller <jonathanmiller2@hotmail.com>
2020-08-18 01:44:58 -0500

    Static files are fixed, home page has been redesigned, about to redo celery implementation

--- app/eomf/celery.py
+++ app/eomf/celery.py
@@ -3,8 +3,8 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celeryq.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eomf.settings")
 
-app = Celery("celeryq")
+app = Celery("eomf")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
