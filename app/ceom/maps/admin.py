9f35da7d94a787d7df2a12d73eed9339daca31eb
Jonathan Miller <jonathanmiller2@hotmail.com>
2022-08-28 04:41:19 +0000

    Set up basics for maps module

--- app/ceom/maps/admin.py
+++ app/ceom/maps/admin.py
@@ -1,27 +1,6 @@
 from django.contrib import admin
+from ceom.maps.models import *
 
-from ceom.maps.models import map_gallery, Comment, poi, roi
-
-
-class CommentInline(admin.TabularInline):
-	model = Comment
-
-class map_galleryAdmin(admin.ModelAdmin):
-	inlines = [
-		CommentInline,
-	]
-
-admin.site.register(map_gallery, map_galleryAdmin)
-
-
-class poiAdmin(admin.ModelAdmin):
-	pass
-
-admin.site.register(poi, poiAdmin)
-
-
-class roiAdmin(admin.ModelAdmin):
-	pass
-
-admin.site.register(roi, roiAdmin)
-# vim: et sw=4 sts=4
+class GeocatterPointAdmin(admin.ModelAdmin):
+    list_display = ('id', 'lat', 'lon', 'category')
+admin.site.register(GeocatterPoint, GeocatterPointAdmin)
