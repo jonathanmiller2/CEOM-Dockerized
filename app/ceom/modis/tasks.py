4227fb9a773b21a8727f04268b3a7041969ccb25
Jonathan Miller <jonathanmiller2@hotmail.com>
2023-01-01 15:06:59 +0000

    Celery optimizations solidified

--- app/ceom/modis/tasks.py
+++ app/ceom/modis/tasks.py
@@ -36,8 +36,14 @@ def process_MODIS_single_site(task_id, media_root, csv_folder, dataset_name, dat
             subtask_param.append(1 / len(subtask_params))
         
         # Chunking vs. non-chunking.
-        # chain(MODIS_fetch_day.chunks(subtask_params, 20).group(), finish_MODIS_single_site.s(task_id, dataset_name, full_file, rel_file)).delay()
-        chain(group(MODIS_fetch_day.s(a, b, c, d, e) for a, b, c, d, e in subtask_params), finish_MODIS_single_site.s(task_id, dataset_name, full_file, rel_file)).delay()
+        # Not chunking smaller tasks is significantly faster, but for larger tasks chunking is slightly slower
+        # TODO: There may be further optimizations here in exactly how the celery queue handles these tasks
+        MAXIMUM_SUBTASK_COUNT = 100
+        if len(subtask_params) <= MAXIMUM_SUBTASK_COUNT:
+            chain(group(MODIS_fetch_day.s(a, b, c, d, e) for a, b, c, d, e in subtask_params), finish_MODIS_single_site.s(task_id, dataset_name, full_file, rel_file)).delay()
+        else:
+            chain(MODIS_fetch_day.chunks(subtask_params, MAXIMUM_SUBTASK_COUNT).group(), finish_MODIS_single_site.s(task_id, dataset_name, full_file, rel_file)).delay()
+        
 
     except Exception as e:
         job_obj.working = False
@@ -77,49 +83,75 @@ def process_MODIS_multiple_site(self, media_root, csv_folder, dataset_name, data
 
 @app.task(bind=True)
 def MODIS_fetch_day(self, task_id, day_file_loc, x, y, job_completion_fraction):
-    day_file_basename = os.path.basename(day_file_loc)
-    file_year = int(day_file_basename.split('.')[1][1:5])
-    file_day = int(day_file_basename.split('.')[1][5:])
-    file_date = date.fromisoformat(str(file_year) + '-01-01') + timedelta(days=file_day - 1)
+    try:
+        day_file_basename = os.path.basename(day_file_loc)
+        file_year = int(day_file_basename.split('.')[1][1:5])
+        file_day = int(day_file_basename.split('.')[1][5:])
+        file_date = date.fromisoformat(str(file_year) + '-01-01') + timedelta(days=file_day - 1)
 
-    ds = gdal.Open(day_file_loc)
+        ds = gdal.Open(day_file_loc)
 
-    subdatasets = ds.GetSubDatasets()
+        subdatasets = ds.GetSubDatasets()
 
-    record = {
-        'date': file_date.strftime('%Y-%m-%d')
-    }
+        record = {
+            'date': file_date.strftime('%Y-%m-%d')
+        }
 
-    for i in range(len(subdatasets)):
-        subdataset_name = subdatasets[i][0].split(':')[4]
-        measurement_ds = gdal.Open(subdatasets[i][0])
-        record[subdataset_name] = measurement_ds.ReadAsArray(int(x), int(y), 1, 1)[0][0].item()
+        for i in range(len(subdatasets)):
+            subdataset_name = subdatasets[i][0].split(':')[4]
+            measurement_ds = gdal.Open(subdatasets[i][0])
+            record[subdataset_name] = measurement_ds.ReadAsArray(int(x), int(y), 1, 1)[0][0].item()
 
-    job_obj = MODISSingleTimeSeriesJob.objects.get(task_id=task_id)
-    job_obj.percent_complete = job_obj.percent_complete + job_completion_fraction
-    job_obj.save()
+        job_obj = MODISSingleTimeSeriesJob.objects.get(task_id=task_id)
+        job_obj.percent_complete = job_obj.percent_complete + job_completion_fraction
+        job_obj.save()
+
+        return record
+
+    except Exception as e:
+        job_obj = MODISSingleTimeSeriesJob.objects.get(task_id=task_id)
+        job_obj.working = False
+        job_obj.completed = False
+        job_obj.error = True
+        job_obj.percent_complete = 1
+        job_obj.save()
+
+        # Re-raise exception
+        raise e
 
     return record
 
 
 @app.task(bind=True)
 def finish_MODIS_single_site(self, records, task_id, dataset_name, full_file, rel_file):
-    # Unpack required if chunking subtasks
-    # records = [item for sublist in records for item in sublist]
+    try:
+        # Unpack required if chunking subtasks
+        if type(records[0]) is list:
+            records = [item for sublist in records for item in sublist]
 
-    job_obj = MODISSingleTimeSeriesJob.objects.get(task_id=task_id)
+        final_df = pd.DataFrame.from_records(records, index='date')
+        final_df = final_df[sort_columns(dataset_name, list(final_df.columns))]
 
-    final_df = pd.DataFrame.from_records(records, index="date")
-    final_df = final_df[sort_columns(dataset_name, list(final_df.columns))]
+        with open(full_file, 'w+') as csvfile:
+            final_df.to_csv(csvfile)
 
-    with open(full_file, 'w+') as csvfile:
-        final_df.to_csv(csvfile)
+        job_obj = MODISSingleTimeSeriesJob.objects.get(task_id=task_id)
+        job_obj.working = False
+        job_obj.completed = True
+        job_obj.percent_complete = 1
+        job_obj.result.name = rel_file
+        job_obj.save()
 
-    job_obj.working = False
-    job_obj.completed = True
-    job_obj.percent_complete = 1
-    job_obj.result.name = rel_file
-    job_obj.save()
+    except Exception as e:
+        job_obj = MODISSingleTimeSeriesJob.objects.get(task_id=task_id)
+        job_obj.working = False
+        job_obj.completed = False
+        job_obj.error = True
+        job_obj.percent_complete = 1
+        job_obj.save()
+
+        # Re-raise exception
+        raise e
 
     return
 
@@ -133,20 +165,19 @@ def sort_columns(dataset_name, column_list):
 
     DESIRED_ORDER = { 
         'MOD09A1': [
-                'date'
-                'sur_refl_b01'
-                'sur_refl_b02'
-                'sur_refl_b03'
-                'sur_refl_b04'
-                'sur_refl_b05'
-                'sur_refl_b06'
-                'sur_refl_b07'
-                'sur_refl_qc_500m'
-                'sur_refl_szen'
-                'sur_refl_vzen'
-                'sur_refl_raz'
-                'sur_refl_state_500m'
-                'sur_refl_day_of_year'
+                'sur_refl_b01',
+                'sur_refl_b02',
+                'sur_refl_b03',
+                'sur_refl_b04',
+                'sur_refl_b05',
+                'sur_refl_b06',
+                'sur_refl_b07',
+                'sur_refl_qc_500m',
+                'sur_refl_szen',
+                'sur_refl_vzen',
+                'sur_refl_raz',
+                'sur_refl_state_500m',
+                'sur_refl_day_of_year',
             ],
     }
 
@@ -158,4 +189,4 @@ def sort_columns(dataset_name, column_list):
             return starting_columns + sorted(column_list)
 
     # The hard-coded sort has succeeded. Returning hard-coded sort.
-    return starting_columns + desired_order
+    return starting_columns + DESIRED_ORDER[dataset_name]
